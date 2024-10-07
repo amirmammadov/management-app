4ade9382cde8e8d9f01673142ade918ad1ff465b
amirmammadov <amirmammado@gmail.com>
2024-10-07 22:01:40 +0400

    ngOnInit lifecycle method was added

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus = 'online';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      let rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'offline';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'unknown';
+      } else {
+        this.currentStatus = 'online';
+      }
+    }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
 }
