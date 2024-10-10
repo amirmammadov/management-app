ec6e9d6561f74a105d1487e17bfddfa5d582c0ee
amirmammadov <amirmammado@gmail.com>
2024-10-10 22:40:21 +0400

    onDestroy lifecycle method was added

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,11 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerStatusComponent implements OnInit {
   currentStatus = 'online';
+  private destroyRef = inject(DestroyRef);
 
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    const interval = setInterval(() => {
       let rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -24,6 +25,10 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'online';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 
   ngAfterViewInit() {
